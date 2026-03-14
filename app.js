2ca8eba235a3ac7634b16c39be8024dd4c7dceb0
lawalfataikola123 <borabannergeorge@gmail.com>
2026-03-14 02:25:58 +0100

    deploy new UI

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
 app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
@@ -11,5 +10,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Repo2APK running on port ${PORT}`);
 });
